246dfe41ffc20b1f78958af61726e68174bc2172
Patrik Caes-Sayrs <heartofgoldfish@gmail.com>
2026-02-16 17:47:50 -0700

    Added Emp interaction with Bar Sign
    There was already content for emp bar sign but no support for it.
    This adds the interaction with emps to make bar signs EmpDisabled.

--- Content.Shared/BarSign/BarSignComponent.cs
+++ Content.Shared/BarSign/BarSignComponent.cs
@@ -12,6 +12,12 @@ public sealed partial class BarSignComponent : Component
     /// </summary>
     [DataField, AutoNetworkedField]
     public ProtoId<BarSignPrototype>? Current;
+
+    /// <summary>
+    /// The bar sign fallback state upon being EMPed.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public ProtoId<BarSignPrototype>? EmpFallback;
 }
 
 [Serializable, NetSerializable]
--- Content.Shared/BarSign/BarSignSystem.cs
+++ Content.Shared/BarSign/BarSignSystem.cs
@@ -1,3 +1,4 @@
+using Content.Shared.Emp;
 using System.Linq;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
@@ -18,6 +19,8 @@ public sealed class BarSignSystem : EntitySystem
         {
             subs.Event<SetBarSignMessage>(OnSetBarSignMessage);
         });
+
+        SubscribeLocalEvent<BarSignComponent, EmpPulseEvent>(OnEmpPulse);
     }
 
     private void OnMapInit(Entity<BarSignComponent> ent, ref MapInitEvent args)
@@ -37,8 +40,31 @@ public sealed class BarSignSystem : EntitySystem
         SetBarSign(ent, signPrototype);
     }
 
+    /// <summary>
+    /// Set the sprite, name and description of the bar sign to a given <see cref="BarSignPrototype"/>.
+    /// </summary>
+    private void OnEmpPulse(Entity<BarSignComponent> ent, ref EmpPulseEvent args)
+    {
+        //AppearanceSystem.TryGetData<bool>(uid, PowerDeviceVisuals.Powered, out var powered, args.Component);
+        if (!_prototypeManager.Resolve(ent.Comp.EmpFallback, out var empFallbackPrototype))
+            return;
+
+        SetBarSign(ent, empFallbackPrototype);
+        args.Affected = true;
+        args.Disabled = true;
+    }
+
+    /// <summary>
+    /// Set the sprite, name and description of the bar sign to a given <see cref="BarSignPrototype"/>.
+    /// </summary>
     public void SetBarSign(Entity<BarSignComponent> ent, BarSignPrototype newPrototype)
     {
+        if (ent.Comp.Current == newPrototype.ID)
+            return;
+
+        if (HasComp<EmpDisabledComponent>(ent))
+            return;
+
         var meta = MetaData(ent);
         var name = Loc.GetString(newPrototype.Name);
         _metaData.SetEntityName(ent, name, meta);
